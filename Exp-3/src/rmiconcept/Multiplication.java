cad5067686f5d88684186730bea46a64d017e245
Anjani Polnati <78461305+polnati-anjani@users.noreply.github.com>
2021-02-06 21:43:54 +0530

    Delete Multiplication.java

--- Exp-3/src/rmiconcept/Multiplication.java
+++
@@ -1,8 +0,0 @@
-package rmiconcept;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Addition extends Remote {
-	public int add(int a,int b) throws RemoteException;
-}
